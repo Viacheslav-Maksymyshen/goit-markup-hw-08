c020501f819ffafbecd3c792c3f8686fc78147f1
Viacheslav-Maksymyshen <slavamax.it@gmail.com>
2022-05-12 15:16:17 +0300

    Test js

--- test2.js
+++ test2.js
@@ -1,16 +1,25 @@
-function checkForSpam(message) {
-  let result;
-  message = message.toLowerCase();
-  if (message.includes('sale')) {
-    result = true;
-  } else if (message.includes('spam')) {
-    result = true;
-  } else {
-    result = false;
-  }
-  console.log(result);
-  return result;
+// function checkForSpam(message) {
+//   let result;
+//   message = message.toLowerCase();
+//   if (message.includes('sale')) {
+//     result = true;
+//   } else if (message.includes('spam')) {
+//     result = true;
+//   } else {
+//     result = false;
+//   }
+//   console.log(result);
+//   return result;
+// }
+// checkForSpam('Get best sale offers now!');
+// checkForSpam('Amazing SalE, only tonight!');
+// checkForSpam('[SPAM] How to earn fast money?');
+
+function getExtremeElements(array) {
+  // Change code below this line
+  const firstElement = array[0];
+  const LastElement = array[array.length - 1];
+  return console.log((array = `[${firstElement}, ${LastElement}]`));
+  // Change code above this line
 }
-checkForSpam('Get best sale offers now!');
-checkForSpam('Amazing SalE, only tonight!');
-checkForSpam('[SPAM] How to earn fast money?');
+getExtremeElements([1, 2, 3, 4, 5]);
